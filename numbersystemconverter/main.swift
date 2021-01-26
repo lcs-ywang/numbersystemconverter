940604b25d6e5f7df1385254be9a4f03d87f950c
yining wang <ywang@lcs.on.ca>
2021-01-26 14:53:39 -0500

    got the prompt for the "form" of the number system

--- numbersystemconverter/main.swift
+++ numbersystemconverter/main.swift
@@ -9,3 +9,20 @@ import Foundation
 
 print("Hello, World!")
 
+//Input
+print("What number system do you want to convert from")
+print("B: Binary")
+print("O: Octal")
+print("D: Decimal")
+print("H: Hexadecimal")
+print("Enter your choice (B/O/ /D/H):", terminator: "")//keep following output on the same line
+
+let from = readLine()!
+
+
+
+//Process
+
+
+//Output
+
