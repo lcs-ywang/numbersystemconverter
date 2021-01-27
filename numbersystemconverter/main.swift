45c36b1d17ba96bc9edb10404816030912bd3e5a
yining wang <ywang@lcs.on.ca>
2021-01-27 11:30:11 -0500

    update jan 27

--- numbersystemconverter/main.swift
+++ numbersystemconverter/main.swift
@@ -20,9 +20,119 @@ print("Enter your choice (B/O/ /D/H):", terminator: "")//keep following output o
 let from = readLine()!
 
 
+print("What number do you want to convert from")
+let value = readLine()!
 
+print("What number do you want to convert to")
+print("B: Binary")
+print("O: Octal")
+print("D: Decimal")
+print("H: Hexadecimal")
+print("Enter your choice (B/O/ /D/H):", terminator: "")
+
+let to = readLine()!
+
+func decimalequivalent(value:String, base:Double) -> Double {
+    var exponent = 0.0
+    var result = 0.0
+    
+    for character in value.reversed() {
+        switch character{
+        case "A":
+            result += 10 * pow(base, exponent)
+        case "B":
+            result += 11 * pow(base, exponent)
+        case "C":
+            result += 12 * pow(base, exponent)
+        case "D":
+            result += 13 * pow(base, exponent)
+        case "E":
+            result += 14 * pow(base, exponent)
+        case "F":
+            result += 15 * pow(base, exponent)
+        case "G":
+            result += 16 * pow(base, exponent)
+        default:
+            let digit = Double(String(character))!
+            result += digit * pow(base, exponent)
+        }
+        exponent += 1
+    }
+    return result
+}
+    
 //Process
 
+var b = 0.0
+switch from {
+case "B":
+    b = 2.0
+case "O":
+    b = 8.0
+case "D":
+    b = 10.0
+case "H":
+    b = 16.0
+default:
+    break
+}
 
+let result = decimalequivalent(value: value, base: b )
 //Output
 
+
+enum NumberSystemBase: Int {
+    case binary = 2
+    case octal = 8
+    case hexadeciml = 16
+    case decimal = 10
+}
+
+func getRepresentation(of valueToConvert: Int, inBase base: NumberSystemBase) -> String {
+    var decimalValueLeftToConvert = valueToConvert
+    var representation = ""
+    while decimalValueLeftToConvert > 0 {
+        let nextDigit = decimalValueLeftToConvert % base.rawValue
+        if base == .hexadeciml {
+            switch nextDigit {
+            case 0...9:
+                representation = String(nextDigit) + representation
+            case 10:
+                representation = "A" + representation
+            case 11:
+                representation = "B" + representation
+            case 12:
+                representation = "C" + representation
+            case 13:
+                representation = "D" + representation
+            case 14:
+                representation = "E" + representation
+            case 15:
+                representation = "F" + representation
+            default:
+                break
+            }
+        } else {
+            representation = String(nextDigit) + representation
+        }
+        decimalValueLeftToConvert = decimalValueLeftToConvert / base.rawValue
+
+    }
+    
+    return representation
+}
+
+
+switch b{
+case 2.0:
+    getRepresentation(of:Int(result), inBase:.binary)
+case 8.0:
+    getRepresentation(of:Int(result), inBase:.octal)
+case 16:
+    getRepresentation(of:Int(result), inBase:.hexadeciml)
+case 10:
+    getRepresentation(of:Int(result), inBase: .decimal)
+
+default:
+    break
+}
